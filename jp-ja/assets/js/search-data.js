0c8a333e3e6627c8e18437be0e44ece5ee618eba
kyoukuntaro <50971798+kyoukuntaro@users.noreply.github.com>
2026-07-12 15:40:37 +0000

    Deploying to gh-pages from @ kyoukuntaro/kyoukuntaro.github.io@a3a452609abb009e7142a6c5f234d957e8896616 🚀

--- jp-ja/assets/js/search-data.js
+++ jp-ja/assets/js/search-data.js
@@ -451,15 +451,13 @@ ninja.data = [{
           description: "",
           section: "",handler: () => {
               window.location.href = "/jp-ja/books/en-us/the_godfather/";
-            },},{id: "news-aistats-2026で発表してきました",
-          title: 'AISTATS 2026で発表してきました。',
+            },},{id: "news-aistats-2026で発表しました",
+          title: 'AISTATS 2026で発表しました。',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-共著論文がcolmにアクセプトされました",
+          title: '共著論文がCOLMにアクセプトされました。',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/jp-ja/news/en-us/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-7",
